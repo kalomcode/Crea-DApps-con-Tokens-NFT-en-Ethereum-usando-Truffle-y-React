8375dedb765bd4ec864b7950f10339b7249152c3
kalomcode <carloscalzadacabano@gmail.com>
2021-11-15 13:06:37 +0100

    Empezando con truffle

---
+++ HELLO/hello.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract hello {
+
+    string public message = "hola mundo";
+
+    function getMessage() public view returns(string memory) {
+        return message;
+    }
+
+    function setMessage(string memory _message) public {
+        message = _message;
+    }
+
+}
